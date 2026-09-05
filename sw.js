177895c7b2b8f87207cb7229eeeb8590fa460131
tomoyukiminato-create <tomoyuki.minato@gmail.com>
2026-09-06 03:37:29 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-
-const CACHE = "hilo-pwa-v6-no-nextbet";
-const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
